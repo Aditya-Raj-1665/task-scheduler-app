9cd98df0b8b22c3b40338e6d8cdcf2f538a964f2
Aditya Raj <adityaraj75621@gmail.com>
2025-11-23 21:40:07 +0530

    simplification of the logic

--- backend/manager.py
+++ backend/manager.py
@@ -98,67 +98,67 @@ class TaskManager:
 
 
 
-    async def create_schedule(self, task: TaskSchedule):
-        print(f"[TaskManager]: Creating new schedule: {task.name}")
-        now = datetime.now(timezone.utc)
-        start_date_utc = task.start_date.astimezone(timezone.utc)
-        iterator_start_time = max(now, start_date_utc)
+    # async def create_schedule(self, task: TaskSchedule):
+    #     print(f"[TaskManager]: Creating new schedule: {task.name}")
+    #     now = datetime.now(timezone.utc)
+    #     start_date_utc = task.start_date.astimezone(timezone.utc)
+    #     iterator_start_time = max(now, start_date_utc)
 
-        try:
-            first_run = croniter(task.cron, iterator_start_time).get_next(datetime)
-        except Exception as e:
-            raise HTTPException(status_code=400, detail=f"Invalid cron string: {e}")
+    #     try:
+    #         first_run = croniter(task.cron, iterator_start_time).get_next(datetime)
+    #     except Exception as e:
+    #         raise HTTPException(status_code=400, detail=f"Invalid cron string: {e}")
 
-        if first_run > task.end_date.astimezone(timezone.utc):
-            raise HTTPException(status_code=400, detail="First run time is after the end date. Task will never run.")
+    #     if first_run > task.end_date.astimezone(timezone.utc):
+    #         raise HTTPException(status_code=400, detail="First run time is after the end date. Task will never run.")
 
-        task_doc = task.model_dump()
-        task_doc["next_run"] = first_run
+    #     task_doc = task.model_dump()
+    #     task_doc["next_run"] = first_run
 
-        await self.db.schedules.insert_one(task_doc)
-        return {"message": "Task schedule created", "first_run": first_run}
-
-
-    async def get_all_schedules(self):
-        """
-        Fetches all task schedules from the MongoDB database.
-        """
-        tasks_list = []
-        due_tasks_cursor = self.db.schedules.find({})
-        async for task in due_tasks_cursor:
-            tasks_list.append(TaskInDB(**task_dict))
-        return tasks_list
+    #     await self.db.schedules.insert_one(task_doc)
+    #     return {"message": "Task schedule created", "first_run": first_run}
 
 
-    async def delete_schedule(self, task_id: str):
-        try:
-            object_id_to_delete = ObjectId(task_id)
-        except Exception:
-            raise HTTPException(status_code=400, detail="Invalid task ID format")
+    # async def get_all_schedules(self):
+    #     """
+    #     Fetches all task schedules from the MongoDB database.
+    #     """
+    #     tasks_list = []
+    #     due_tasks_cursor = self.db.schedules.find({})
+    #     async for task in due_tasks_cursor:
+    #         tasks_list.append(TaskInDB(**task_dict))
+    #     return tasks_list
 
-        delete_result = await self.db.schedules.delete_one({"_id": object_id_to_delete})
+
+    # async def delete_schedule(self, task_id: str):
+    #     try:
+    #         object_id_to_delete = ObjectId(task_id)
+    #     except Exception:
+    #         raise HTTPException(status_code=400, detail="Invalid task ID format")
+
+    #     delete_result = await self.db.schedules.delete_one({"_id": object_id_to_delete})
         
-        if delete_result.deleted_count == 1:
-            return {"message": "Task deleted successfully"}
-        else:
-            raise HTTPException(status_code=404, detail="Task not found")
+    #     if delete_result.deleted_count == 1:
+    #         return {"message": "Task deleted successfully"}
+    #     else:
+    #         raise HTTPException(status_code=404, detail="Task not found")
 
 
-    async def fun_done(self):
-        while True:
-            await asyncio.sleep(30) 
+    # async def fun_done(self):
+    #     while True:
+    #         await asyncio.sleep(30) 
             
-            print(f"[TaskManager Loop 3]: Waking up to clean 'queue_table' of completed tasks...")
+    #         print(f"[TaskManager Loop 3]: Waking up to clean 'queue_table' of completed tasks...")
             
-            try:
-                delete_result = await self.db.queue_table.delete_many(
-                    {"status": "completed"}
-                )
-                if delete_result.deleted_count > 0:
-                    print(f"[TaskManager Loop 3]: Cleaned up {delete_result.deleted_count} completed tasks.")
-                else:
-                    print(f"[TaskManager Loop 3]: No completed tasks to clean up.")
-            except Exception as e:
-                print(f"[TaskManager Loop 3]: ERROR during cleanup: {e}")
+    #         try:
+    #             delete_result = await self.db.queue_table.delete_many(
+    #                 {"status": "completed"}
+    #             )
+    #             if delete_result.deleted_count > 0:
+    #                 print(f"[TaskManager Loop 3]: Cleaned up {delete_result.deleted_count} completed tasks.")
+    #             else:
+    #                 print(f"[TaskManager Loop 3]: No completed tasks to clean up.")
+    #         except Exception as e:
+    #             print(f"[TaskManager Loop 3]: ERROR during cleanup: {e}")
 
 
